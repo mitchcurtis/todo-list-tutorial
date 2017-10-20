f48d2d599957b0541ead01a693129bdee21a7e12
Mitch Curtis <mitch.curtis@qt.io>
2017-10-20 13:13:58 +0200

    Add chapter-9 files

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "todolist.h"
+#include "todomodel.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<ToDoModel>("ToDo", 1, 0, "ToDoModel");
+
+    ToDoList toDoList;
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty(QStringLiteral("toDoList"), &toDoList);
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
