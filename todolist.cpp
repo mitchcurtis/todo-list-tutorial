a0b005230f76b6bf9d652ef80a86c497d6f00d4f
Mitch Curtis <mitch.curtis@qt.io>
2017-10-20 13:13:52 +0200

    Add chapter-8 files

---
+++ todolist.cpp
@@ -0,0 +1,51 @@
+#include "todolist.h"
+
+ToDoList::ToDoList(QObject *parent) : QObject(parent)
+{
+    mItems.append({ true, QStringLiteral("Wash the car") });
+    mItems.append({ false, QStringLiteral("Fix the sink") });
+}
+
+QVector<ToDoItem> ToDoList::items() const
+{
+    return mItems;
+}
+
+bool ToDoList::setItemAt(int index, const ToDoItem &item)
+{
+    if (index < 0 || index >= mItems.size())
+        return false;
+
+    const ToDoItem &oldItem = mItems.at(index);
+    if (item.done == oldItem.done && item.description == oldItem.description)
+        return false;
+
+    mItems[index] = item;
+    return true;
+}
+
+void ToDoList::appendItem()
+{
+    emit preItemAppended();
+
+    ToDoItem item;
+    item.done = false;
+    mItems.append(item);
+
+    emit postItemAppended();
+}
+
+void ToDoList::removeCompletedItems()
+{
+    for (int i = 0; i < mItems.size(); ) {
+        if (mItems.at(i).done) {
+            emit preItemRemoved(i);
+
+            mItems.removeAt(i);
+
+            emit postItemRemoved();
+        } else {
+            ++i;
+        }
+    }
+}
